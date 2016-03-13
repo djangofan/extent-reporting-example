fe98cb7232ddd6f82a1e8cc6c4c1c3277e520248
Jon Austen <djangofan@gmail.com>
2016-03-13 15:59:54 -0700

    Changed from listener based report to annotated based report.

--- src/main/java/qa/test/listeners/ExtentNGSuiteReporter.java
+++ src/main/java/qa/test/listeners/ExtentNGSuiteReporter.java
@@ -10,17 +10,10 @@ import java.util.Date;
 
 public class ExtentNGSuiteReporter implements ITestListener, ISuiteListener, IExecutionListener, IInvokedMethodListener
 {
-    private ExtentReports extentReports;
-    private ExtentTest extentTest;
-
-    private final String reportLocation = "build" + File.separator + "reports" + File.separator + "extent"
-            + File.separator + "index.html";
-
     @Override
     public void onExecutionStart()
     {
         //Invoked before the TestNG run starts.
-        extentReports = new ExtentReports(reportLocation, true, NetworkMode.OFFLINE);
     }
 
     @Override
@@ -41,59 +34,40 @@ public class ExtentNGSuiteReporter implements ITestListener, ISuiteListener, IEx
         // invoked right before test method
     }
 
-
     @Override
     public void afterInvocation(IInvokedMethod method, ITestResult testResult)
     {
         // invoked right after test method
-        ITestNGMethod iTestNGMethod = method.getTestMethod();
-        ITestClass iTestClass = iTestNGMethod.getTestClass();
-        String annotatedTestName = iTestClass.getTestName();
-        testResult.setAttribute("annotatedTestName", annotatedTestName);
-        String testName = (String)testResult.getAttribute("annotatedTestName");
-        extentTest = extentReports.startTest(testName, "Description: -------");
-        extentTest.log(LogStatus.INFO, "Starting test " + testName + " ...");
     }
 
     @Override
     public void onTestStart(ITestResult result)
     {
         //Invoked each time before a test will be invoked
-        extentTest.setStartedTime(new Date());
     }
 
     @Override
     public void onTestSuccess(ITestResult result)
     {
         //Invoked each time a test succeeds.
-        extentTest.log(LogStatus.PASS, "Test Passed.");
-        extentTest.setEndedTime(new Date());
     }
 
     @Override
     public void onTestFailure(ITestResult result)
     {
         //Invoked each time a test fails.
-        extentTest.log(LogStatus.FAIL, "Test Failed.");
-        extentTest.setEndedTime(new Date());
     }
 
     @Override
     public void onTestSkipped(ITestResult result)
     {
         //Invoked each time a test is skipped.
-        extentTest.log(LogStatus.SKIP, "Test Skipped.");
-        extentTest.setEndedTime(new Date());
     }
 
     @Override
     public void onTestFailedButWithinSuccessPercentage(ITestResult result)
     {
         //Invoked each time a method fails but has been annotated with successPercentage and this failure still keeps it within the success percentage requested.
-        extentTest.log(LogStatus.FAIL, "Test Failed By Test Percentage.");
-        extentTest.setEndedTime(new Date());
-        extentReports.endTest(extentTest);  // ???
-        extentReports.flush();
     }
 
     @Override
@@ -113,8 +87,6 @@ public class ExtentNGSuiteReporter implements ITestListener, ISuiteListener, IEx
     public void onExecutionFinish()
     {
         //Invoked once all the suites have been run.
-
-        extentReports.close();
     }
 
 }
--- src/test/java/qa/test/ExtentTestBase.java
+++ src/test/java/qa/test/ExtentTestBase.java
@@ -1,20 +1,76 @@
 package qa.test;
 
-import org.apache.commons.lang3.StringUtils;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+import com.relevantcodes.extentreports.NetworkMode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.ITest;
 import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.lang.reflect.Method;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public abstract class ExtentTestBase implements ITest
 {
     private String testName;
+    private ExtentReports extentReport;
+    private ExtentTest extentTest;
+
+    private final String reportLocation = "build" + File.separator + "extent-report" + File.separator + "index.html";
+    private final Logger logger = LoggerFactory.getLogger(this.getClass().getCanonicalName());
+
+    @BeforeClass
+    public void setupClass()
+    {
+        createReportInstance();
+    }
+
+    @BeforeMethod
+    public void setupBeforeTestMethod(Method method, ITestResult iTestResult)
+    {
+        setTestName(method.getName());
+        String description = method.getAnnotation(Test.class).description();
+        extentTest = extentReport.startTest(getTestName());
+        extentTest.setDescription(description);
+        extentTest.setStartedTime(new Date());
+        extentTest.log(LogStatus.INFO, "Starting test " + getTestName() + " ...");
+    }
+
+    @AfterMethod
+    public void teardownAfterTestmethod(ITestResult iTestResult)
+    {
+        int testngStatus = iTestResult.getStatus();
+        logger.info("TestNG test status after test: " + testngStatus);
+        if ( testngStatus == ITestResult.SUCCESS ) {
+            extentTest.log(LogStatus.PASS, "Test Passed.");
+        } else if ( testngStatus == ITestResult.FAILURE ) {
+            extentTest.log(LogStatus.FAIL, "Test Failed.");
+        } else if ( testngStatus == ITestResult.SKIP ) {
+            extentTest.log(LogStatus.SKIP, "Test Skipped.");
+        } else {
+            extentTest.log(LogStatus.ERROR, "Test problem.");
+        }
+        extentTest.setEndedTime(new Date());
+        flushTestToReport(extentReport, extentTest);
+    }
+
+    public void teardownClass()
+    {
+        extentReport.close(); // report html wont show up until this is called
+    }
 
     public String getTestName()
     {
-        if (StringUtils.isBlank(this.testName))
-        {
-            this.testName = "undefined";
-        }
         return this.testName;
     }
 
@@ -23,10 +79,23 @@ public abstract class ExtentTestBase implements ITest
         this.testName = name;
     }
 
-    @BeforeMethod
-    public void setupBeforeTestMethod(ITestResult iTestResult)
+    private ExtentReports createReportInstance() {
+        extentReport = new ExtentReports(reportLocation, false, NetworkMode.OFFLINE);
+        extentReport.addSystemInfo("systemOs", "Windows");
+        return extentReport;
+    }
+
+    private void flushTestToReport(ExtentReports extent, ExtentTest test)
     {
-        setTestName((String)iTestResult.getAttribute("annotatedTestName"));
+        extent.endTest(test);
+        extent.flush();
+    }
+
+    private String getDateTime() {
+        Date date = Calendar.getInstance().getTime();
+        DateFormat formatter = new SimpleDateFormat("dd-MM-yyyy_HH_mm_ss");
+        String today = formatter.format(date);
+        return today;
     }
 
 }
